4be552878777f54fbc958e18ab5ffc6e15a7a566
Suriah Kamaruddin <suriahkamaruddin@gmail.com>
2022-04-14 12:44:44 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://kasatria.herokuapp.com';
+$config['base_url'] = 'https://kasatria.herokuapp.com/three_js_periodic_table';
 
 /*
 |--------------------------------------------------------------------------
